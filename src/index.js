d56b4c33bd76c482f6fef24cf9dbd10daff7691e
Harsh <hparashar617@gmail.com>
2022-07-17 12:56:47 +0530

    all changes saved

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+//   document.getElementById('root')
+// );
+ReactDOM.render((
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+), document.getElementById('root'));
